b7a85cffc45c7ed07f0de82d662ecce1dcf084f9
Emerson Demetrio <emerson.demetrio@scytech.com.br>
2017-08-14 16:23:54 -0300

    client is now configurable

--- client/client.js
+++ client/client.js
@@ -1,9 +1,19 @@
 const net = require("net");
 const client = new net.Socket();
-const config = require("../configuration.json");
+const args = process.argv;
+
+const config = {
+    host: args[2] || 5.83.10.123,
+    port: args[3] || 3030,
+    parser: args[4] || "none"
+}
 
 const parseTCPData = (data) => {
-	return JSON.parse(data);
+    if(config.parser !== "none"){
+        return JSON.parse(data.toString());
+    } else {
+        return data.toString();
+    }
 }
 
 client.connect(config.port, config.host, function() {
